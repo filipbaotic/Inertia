5bfb26e69db17e05c38ae0f9bd374d3066c121d9
Filip Baotić <filip30stm@gmail.com>
2019-09-20 22:08:16 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
     var bar = $(".hamburger");
     var links = $(".mobile_header");
     var logo = $("a.logo");
+    var current = $(".current");
 
     $(".hamburger").click(function(e) {
         e.preventDefault();
         bar.toggleClass("active");
         links.toggleClass("active");
         logo.toggleClass("active");
+        current.toggleClass("active");
     });
 });
