c38368e21324363c14daebb277528dbad9dec1d8
Filip Baotić <filip30stm@gmail.com>
2019-09-21 03:41:52 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,3 +12,23 @@ $(document).ready(function() {
         current.toggleClass("active");
     });
 });
+
+(function($) {
+    $.fn.parlx = function(options) {
+        var options = $.extend({
+            speed: 0.08
+        }, options);
+        return this.each(function() {
+            const backImage = $(this);
+            let speed = options.speed;
+            if (speed > 0.5 || speed < 0.1) {
+                speed = 0.08;
+            }
+            const scrolled = $(window).scrollTop() - backImage.parent().offset().top;
+            backImage.css({ "background-position": "center calc(60% - " + speed * scrolled + "px)" });
+        });
+    }
+    $(window).on("load resize scroll", function() {
+        $('.prx').parlx();
+    });
+})(jQuery);
